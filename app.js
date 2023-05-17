d1cad8eade349db377bba5ae5ec5843ede47ea3e
Akunov4594 <frontdevkg@gmail.com>
2023-05-17 18:38:01 +0600

    lesson-1

---
+++ app.js
@@ -0,0 +1,64 @@
+// ---.js
+
+
+//  Hello World
+//  " "  v1
+//  'I'm  '   v2
+//  ` `   v3
+
+//  '  lorem "ipsum dolor"  '
+
+//  " I'm  don't  "
+
+// Shift + Alt +  -> down 
+
+// Типы данных (8)
+
+// Number 1
+//  var -> variable 
+var number1 = 100
+var age = 18
+var usd = 85.45
+usd = 87.45
+var temp = -10
+// string 2
+var userName = "Kanat"
+// boolean (true | false)  3
+var hasCar = false
+var isTrue = 8 > 44 //  false
+// undefined  4
+var empty; // undefined
+// null 5
+var nullExp = null
+// Symbol 6 --------------
+var sym = Symbol('id')
+// object 7  {} []
+var car = {
+  //key : value
+  // ключ : значение
+  model: 'BMW',
+  color: 'black',
+  price: 200000,
+  year: 2015
+}  
+// bigInt 8  ------------
+var bInt  =  4n
+// array массив 
+var movies = ['Avatar 2', 'Chas Pik', 'Jon Wik', 'Forsaj 10']
+
+console.log(car);
+
+var parents = {
+  fatherName: 'Dad Name',
+  motherName: 'Mom Name',
+  fatherAge: 48,
+  motherAge: 45,
+}
+
+var kol = {
+  img: 'https://img5.lalafo.com/i/posters/api_webp/64/c9/f0/bf12222e55412827ee0042789f.webp',
+  img2: 'url ',
+  price: 6000,
+  description: 'lorem ipsum dolor',
+  satuuchu: 'Solnyshko'
+}
